6fe312192194a96c7ea34635b4c96df84691125e
Diego Artur <71456032+odiegoartur@users.noreply.github.com>
2024-05-15 20:27:14 +0000

    adciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>Flavia, eu amo você. Se você me ama, dá uma risadinha!</h1>
-
+function Home() {
+  return <h1>Flavia, eu amo você. Se você me ama, dá uma risadinha!</h1>;
 }
 
 export default Home;
